f5a12498478d079d201c0032e1c02fe29385e8d9
Rajat Kalotra <rajatraguvanshi3900@gmail.com>
2022-07-01 01:33:23 +0530

    Done

--- src/Components/Todocard.js
+++ src/Components/Todocard.js
@@ -80,29 +80,39 @@ export default function Todocard() {
         <>
 
             {/* Main Div */}
-            <div className='bg-white min-h-screen w-full flex justify-center items-center ' >
+            <div className='bg-white min-h-screen w-full flex-col justify-center items-center ' >
 
                 {/* Card */}
-                <div className='  w-4/6 min-h-screen py-4 shadow-2xl' >
+                <div className='  w-5/6 min-h-screen py-4 mx-auto shadow-2xl' >
 
                     {/* Heading */}
-                    <div className=' py-4 bg-indigo-900  rounded-t-xl ' >
+                    <div className=' py-4 bg-indigo-900 flex justify-around   rounded-t-xl ' >
                         <h1 className="text-center text-white text-4xl font-bold">Todo App</h1>
+
+
+                        {/* //Clear All button */}
+                        <div onClick={removeAll} className="flex space-x-2 justify-center">
+                            <button type="button" className="inline-block px-6 py-2.5 bg-white text-indigo-900 font-bold rounded-md hover:bg-indigo-100">Clear all</button>
+                        </div>
+
                     </div>
 
                     {/* Input */}
-                    <div className='py-8 px-6 flex justify-center ' >
-                        <input type="text"
+                    <div className='py-8 px-6 flex-col justify-center items-center ' >
+                        <div className='py-2' >
+                            <input type="text"
 
-                            // Getting value using hooks
-                            value={userData}
-                            onChange={(event) => { setuserData(event.target.value) }}
-                            onKeyPress={addItem}
+                                // Getting value using hooks
+                                value={userData}
+                                onChange={(event) => { setuserData(event.target.value) }}
+                                onKeyPress={addItem}
 
-                            id="name" name="name" placeholder='Add items...' className="w-4/5 mx-1 bg-gray-100 bg-opacity-50 rounded border-2 border-indigo-400 focus:border-indigo-500 focus:bg-white text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                id="name" name="name" placeholder='Add items...' className="w-full py-2 px-6 bg-gray-100 bg-opacity-50 rounded border-2 border-indigo-800 focus:border-indigo-900 focus:bg-white text-base outline-none text-gray-700 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
 
-                        {/* //Clear All button */}
-                        <button onClick={removeAll} className='border-2 font-medium mx-1 py-2 px-6 rounded-md border-indigo-400 hover:bg-indigo-100' >Clear All</button>
+
+
+
                     </div>
 
 
@@ -116,7 +126,7 @@ export default function Todocard() {
                         <div
                             className="border-2 flex mx-auto shadow-xl px-3 hover:bg-indigo-100 w-5/6 border-indigo-800 rounded-lg bg-white my-2  sm:flex sm:flex-row justify-around items-center">
                             <div className=" flex justify-center my-2 font-bold">
-                                <p className=" py-2 t ext-lg font-medium">This is a sample Task</p>
+                                <p className=" py-2 text-lg font-medium">This is a sample Task</p>
                             </div>
                             <div className='hidden'  >
                                 <svg className='h-6 w-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path className='fill-indigo-600' d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z" /></svg>
@@ -133,7 +143,7 @@ export default function Todocard() {
                                         onClick={() => myFunc(ele.id, ele.name)}
                                         className="border-2 hover:cursor-pointer flex mx-auto shadow-xl px-3 hover:bg-indigo-100 w-5/6 border-purp;e-800 rounded-lg bg-white my-2  sm:flex sm:flex-row justify-around items-center">
                                         <div className=" flex justify-center my-2 font-bold">
-                                            <p className=" py-2 t ext-lg font-medium">{ele.name}</p>
+                                            <p className=" py-2 text-lg font-medium">{ele.name}</p>
                                         </div>
                                         <div className='hidden'  >
                                             <svg className='h-6 w-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path className='fill-indigo-600' d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z" /></svg>
@@ -156,8 +166,8 @@ export default function Todocard() {
                         {/* Sample Completed  Task to do */}
                         <div
                             className="border-2 flex mx-auto shadow-xl px-3 hover:bg-indigo-100 w-5/6 border-indigo-800 rounded-lg bg-white my-2  sm:flex sm:flex-row justify-around items-center">
-                            <div className=" flex justify-center my-2 font-bold">
-                                <p className=" py-2 t ext-lg font-medium">This is a sample Task</p>
+                            <div className=" flex justify-center items-center my-2 font-bold">
+                                <p className=" py-2 text-lg font-medium">This is a sample Task</p>
                             </div>
                             <div className=''  >
                                 <svg className='h-6 w-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path className='fill-indigo-600' d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z" /></svg>
@@ -182,6 +192,11 @@ export default function Todocard() {
                         }
                     </div>
 
+                </div>
+                <div className=' py-4 mb-4 bg-indigo-900 w-5/6 mx-auto rounded-b-xl ' >
+                    <p className="text-center text-white text-4xl font-bold">
+                        <a className='text-center text-white text-xl font-light' href="https://workwithrajat.netlify.app/">@workwithrajat</a>
+                    </p>
                 </div>
             </div >
 
